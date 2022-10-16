9c18b2a0cd9c4f21ca444f652513dfe6afd9c845
Nicko Cruz M <nickocruzm@gmail.com>
2022-10-15 23:37:14 -0700

    Declared Default constructor and attribute

--- FileManager/fileMan.hpp
+++ FileManager/fileMan.hpp
@@ -1,6 +1,17 @@
 #ifndef _FILE_MAN_HPP_
 #define _FILE_MAN_HPP_
 
+#include <iostream>
+#include <string>
+#include <vector>
 
+using std::string; 
+using std::vector;
+class File_Man{
+    protected:
+        vector<string> fileNames;
+    public:
+        File_Man(); // Default Constructor
+};
 
 #endif
