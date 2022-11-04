a7d874035f1a664d856ce52b14c9016b7a597cbc
Nicko Cruz M <nickocruzm@gmail.com>
2022-11-03 22:43:55 -0700

    cleaning

--- main.cpp
+++ main.cpp
@@ -1,10 +1,5 @@
 #include "FileManager/FileManager.hpp"
 
 int main(){
-    FileManager* f = new FileManager();
-    std::string f_name = "test.txt";
 
-    f->add_file("test.txt");
-
-    f->read(f_name);
 }
