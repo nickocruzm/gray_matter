d8f1c9c9a2ce306ab786f1719a96953488e4858b
Nicko Cruz M <nickocruzm@gmail.com>
2022-11-04 17:03:07 -0700

    changes

--- headers.hpp
+++ headers.hpp
@@ -3,8 +3,15 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
 
 using std::cout;
 using std::endl;
+using std::string;
+using std::vector;
+using std::ifstream;
+
+
 
 #endif
