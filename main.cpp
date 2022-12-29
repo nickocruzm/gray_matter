c77046e22a6422ccf5551c1f4a4959c4e71fd084
Nicko Cruz M <nickocruzm@gmail.com>
2022-12-28 20:49:30 -0800

    testing brain set

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "FileManager/FileManager.hpp"
+#include "Brain/Brain.hpp"
+
 
 int main(){
     vector<string> vec;
+    Brain brain = Brain();
     FileManager f = FileManager();
     std::string f_name = "test.txt";
 
     f.add_file("test.txt");
 
     vec = f.read();
+    brain.set_data(vec);
+
+    cout<< brain.count_uniqueWords() << endl;
 
 }
